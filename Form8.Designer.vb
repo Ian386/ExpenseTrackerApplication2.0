1b3b258258cd34cdc2c5d81e836e00e98f31baab
David Nzambuli <david.nzambuli@students.jkuat.ac.ke>
2024-03-19 00:08:20 +0300

    Added the basic UI for the settings form

--- Form8.Designer.vb
+++ Form8.Designer.vb
@@ -22,15 +22,372 @@ Partial Class Form8
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        PictureBox1 = New PictureBox()
+        TextBox1 = New TextBox()
+        GroupBox1 = New GroupBox()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Label7 = New Label()
+        Label8 = New Label()
+        GroupBox2 = New GroupBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        GroupBox3 = New GroupBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        Label2 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Button3 = New Button()
+        Label11 = New Label()
+        Button4 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 150)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Profile Settings"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 460)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(94, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Security Settings"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 401)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(77, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "View Settings"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(66, 31)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(107, 92)
+        PictureBox1.TabIndex = 4
+        PictureBox1.TabStop = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(6, 42)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(231, 38)
+        TextBox1.TabIndex = 5
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(TextBox5)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(300, 150)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(581, 217)
+        GroupBox1.TabIndex = 6
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "My Details"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(6, 24)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(80, 15)
+        Label5.TabIndex = 7
+        Label5.Text = "Display Name"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(6, 103)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(88, 15)
+        Label6.TabIndex = 8
+        Label6.Text = "Phone Number"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(6, 121)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(231, 38)
+        TextBox2.TabIndex = 9
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(6, 178)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(89, 33)
+        Button1.TabIndex = 10
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(179, 92)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(146, 22)
+        Button2.TabIndex = 11
+        Button2.Text = "Change Profile Photo"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(179, 48)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(228, 38)
+        TextBox3.TabIndex = 11
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(328, 42)
+        TextBox4.Multiline = True
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(228, 38)
+        TextBox4.TabIndex = 11
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(328, 121)
+        TextBox5.Multiline = True
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(228, 38)
+        TextBox5.TabIndex = 12
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(328, 19)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(81, 15)
+        Label7.TabIndex = 13
+        Label7.Text = "Email Address"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(328, 103)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(92, 15)
+        Label8.TabIndex = 14
+        Label8.Text = "Mailing Address"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Location = New Point(300, 401)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(581, 43)
+        GroupBox2.TabIndex = 12
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Appearance"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(6, 18)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(52, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Light"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(328, 18)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(49, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Dark"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(Button3)
+        GroupBox3.Controls.Add(Label2)
+        GroupBox3.Controls.Add(Label9)
+        GroupBox3.Controls.Add(Label10)
+        GroupBox3.Controls.Add(TextBox8)
+        GroupBox3.Controls.Add(TextBox6)
+        GroupBox3.Controls.Add(TextBox7)
+        GroupBox3.Location = New Point(300, 470)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(581, 232)
+        GroupBox3.TabIndex = 13
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Change Password"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(178, 76)
+        TextBox6.Multiline = True
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(231, 38)
+        TextBox6.TabIndex = 15
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(178, 22)
+        TextBox7.Multiline = True
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(231, 38)
+        TextBox7.TabIndex = 16
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(178, 132)
+        TextBox8.Multiline = True
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(231, 38)
+        TextBox8.TabIndex = 17
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(18, 135)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 15)
+        Label2.TabIndex = 14
+        Label2.Text = "View Settings"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(18, 79)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(77, 15)
+        Label9.TabIndex = 15
+        Label9.Text = "View Settings"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(18, 25)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(77, 15)
+        Label10.TabIndex = 16
+        Label10.Text = "View Settings"
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(6, 193)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(89, 33)
+        Button3.TabIndex = 15
+        Button3.Text = "Submit"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(12, 736)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(85, 15)
+        Label11.TabIndex = 14
+        Label11.Text = "Wallet Settings"
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(300, 727)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(89, 33)
+        Button4.TabIndex = 18
+        Button4.Text = "Edit Wallets"
+        Button4.UseVisualStyleBackColor = True
         ' 
         ' Form8
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(957, 772)
+        Controls.Add(Button4)
+        Controls.Add(Label11)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(TextBox3)
+        Controls.Add(Button2)
+        Controls.Add(GroupBox1)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label1)
+        Margin = New Padding(2, 2, 2, 2)
         Name = "Form8"
         Text = "Form8"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button4 As Button
 End Class
--- Form8.vb
+++ Form8.vb
@@ -2,4 +2,40 @@
     Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
 End Class
